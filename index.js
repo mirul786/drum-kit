4b10e04dbf64ed0b6f49bd1da6a43e389031ded3
Mirul Ansari <121329211+mirul786@users.noreply.github.com>
2023-04-11 16:44:42 +0530

    files added to the main page

---
+++ index.js
@@ -0,0 +1,79 @@
+// document.querySelector("button").addEventListener("click", handleClick) // here instead of calling the function as handleClick(), we call it without prothensesis {"without()"==> handleClick}. This is because if we call the function as "handleClick()" then the compiler will read all the html file and run the code unanimously to indefenite amount of time and your browser might get hangover. for a clear understand change the code abobe as "handleClick()" in place of 
+
+
+// function handleClick() {
+//     alert("I got clciked")
+// }
+
+// Now the above line of code is only aplicable to the first button. So down below is how we can add an eventListener to all of the button throug loops.
+
+
+// Detecting Button Press
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrums; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+        var buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML)
+        
+    });
+}
+
+// Detecting Keyboard Depression
+
+document.addEventListener("keydown", function (event) {
+
+    makesound(event.key)
+    
+});
+
+function makesound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+
+            break;
+
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+
+            break;
+        case "k":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+
+            break;
+
+        case "l":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+
+            break;
+
+        default: console.log(buttonInnerHTML);
+        
+    }
+    
+}
